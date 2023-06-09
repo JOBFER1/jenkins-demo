c0cc69a35a106fb843225c4d7d5d996b17297bc3
JOBFER1 <jobfer@outlook.es>
2023-06-09 15:49:31 +0200

    Añade 100% de cobertura en ramas (condiciones)

--- src/test/java/com/bribebybytes/OddEvenTest.java
+++ src/test/java/com/bribebybytes/OddEvenTest.java
@@ -10,12 +10,14 @@ public class OddEvenTest {
 	public void evenTest() {
 		OddEven meo = new OddEven();
         assertEquals("Even number", true, meo.isEvenNumber(14));
+        assertEquals("Even number", false, meo.isEvenNumber(13));
 	}
 	
 	@Test
 	public void oddTest() {
 		OddEven meo = new OddEven();
         assertEquals("Odd number", true, meo.isOddNumber(13));
+        assertEquals("Odd number", false, meo.isOddNumber(14));
 	}
 
 }
